866db00e19e718979c95da5d61a9102276bba743
Jeesu Park <jeesupark96@gmail.com>
2022-10-19 00:09:19 +0000

    revisions

--- js/main.js
+++ js/main.js
@@ -9,28 +9,28 @@ beginbut.addEventListener('click', function () {
   infopage.className = 'find-info';
 });
 
-function getInfo(name) {
+function getName(name) {
+
   event.preventDefault();
   var userlist = document.querySelector('#user-list');
 
-  const data = null;
-
   const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
+  xhr.withCredentials = false;
 
   xhr.addEventListener('readystatechange', function () {
     if (this.readyState === this.DONE) {
-      console.log(this.responseText);
+      console.log(this.json);
     }
   });
-  var targetUrl = encodeURIComponent('https://api-nba-v1.p.rapidapi.com/players?name=Williams&team=1&season=2021');
+  var targetUrl = encodeURIComponent('https://api-nba-v1.p.rapidapi.com/players?name=James');
 
   xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
   xhr.setRequestHeader('x-rapidapi-key', '1b8d6d3d44msh6a42d044a856858p1a0c39jsn1d0dcdaecf3d');
   xhr.setRequestHeader('x-rapidapi-host', 'api-nba-v1.p.rapidapi.com');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    console.log(xhr.response);
+    console.log(xhr.response.response);
+    console.log(xhr);
   });
 
   // xhr.open('GET', 'https://api-nba-v1.p.rapidapi.com/players?name=Williams&team=1&season=2021');
@@ -38,66 +38,90 @@ function getInfo(name) {
   // xhr.setRequestHeader('x-rapidapi-key', '1b8d6d3d44msh6a42d044a856858p1a0c39jsn1d0dcdaecf3d');
   // xhr.setRequestHeader('x-rapidapi-host', 'api-nba-v1.p.rapidapi.com');
 
-  // xhr.send();
+  xhr.send();
   console.log(xhr);
   console.log(data);
 
   xhr.addEventListener('load', function () {
+    console.log(xhr.response.response.length);
+    const player = xhr.response.response;
+    for (let i = 0; i <= player.length; i++) {
 
-    for (let i = 0; i < xhr.response.response.length; i++) {
-
-      event.preventDefault();
-      const player = xhr.response;
       var objectOne = {
-        Name: 'Name: ' + player[i].firstName + ' ' + player[i].lastName,
-        Height: 'Height: ' + player[i].heightInMeters,
-        Weight: 'Weight: ' + player[i].weightInKilograms,
-        YearsPro: 'Years Pro: ' + player[i].yearsPro,
-        Education: 'Birth Place: ' + player[i].affiliation,
-        Jerseynum: 'Jersey Number: ' + player[i].leagues.standard.jersey,
-        Position: 'Position: ' + player[i].leagues.standard.pos,
-        College: 'College ' + player[i].collegeName
+        Name: player[i].firstname + ' ' + player[i].lastname,
+        ID: player[i].id,
+        Height: 'Height: ' + player[i].height.feets + '"' + player[i].height.inches,
+        Weight: 'Weight: ' + player[i].weight.pounds,
+        YearsPro: 'Years Pro: ' + player[i].nba.pro,
+        Education: 'Education: ' + player[i].college,
+        College: 'College ' + player[i].college
       };
       data.entries.unshift(objectOne);
-      form.reset();
+
       console.log(objectOne);
-      playerinfo.appendChild(userlist);
 
       form.className = 'hidden';
-      const team = document.createElement('ul');
       const list = document.createElement('ul');
-      list.textContent = 'Name: ' + player[i].firstName + ' ' + player[i].lastName;
+      list.setAttribute('id', 'playername');
+      list.textContent = objectOne.Name;
       const height = document.createElement('ul');
-      height.textContent = 'Height: ' + player[i].heightInMeters;
-
+      height.textContent = objectOne.Height;
       const weight = document.createElement('ul');
-      weight.textContent = 'Weight: ' + player[i].weightInKilograms;
+      weight.textContent = objectOne.Weight;
       const years = document.createElement('ul');
-      years.textContent = 'Years Pro: ' + player[i].yearsPro;
+      years.textContent = objectOne.YearsPro;
       const education = document.createElement('ul');
-      education.textContent = 'Birth Place: ' + player[i].affiliation;
+      education.textContent = objectOne.Education;
       const jerseynum = document.createElement('ul');
-      jerseynum.textContent = 'Jersey Number: ' + player[i].leagues.standard.jersey;
+      jerseynum.textContent = objectOne.Jerseynum;
       const position = document.createElement('ul');
-      position.textContent = 'Position: ' + player[i].leagues.standard.pos;
+      position.textContent = objectOne.Position;
       const college = document.createElement('ul');
-      college.textContent = 'College ' + player[i].collegeName;
-      playerinfo.appendChild(jerseynum);
-      playerinfo.appendChild(position);
-      playerinfo.appendChild(college);
-      playerinfo.appendChild(education);
-      playerinfo.appendChild(years);
-      playerinfo.appendChild(weight);
-      playerinfo.appendChild(team);
-      playerinfo.appendChild(height);
-      userlist.appendChild(list);
+      college.textContent = objectOne.College;
+      playerinfo.appendChild(list);
+      // playerinfo.appendChild(height);
+      // playerinfo.appendChild(weight);
+      // playerinfo.appendChild(years);
+      // playerinfo.appendChild(college);
+      // playerinfo.appendChild(education);
+      // playerinfo.appendChild(jerseynum);
+      // playerinfo.appendChild(position);
+      form.reset();
     }
+  }
 
-  });
+  );
   infointerior.className = 'hidden';
 
   return playerinfo;
 
 }
+console.log(data.entries);
 
-form.addEventListener('submit', getInfo);
+form.addEventListener('submit', getName);
+
+const playername = document.querySelector('#playername');
+function getInfo(event) {
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = false;
+
+  for (let i = 0; i < data.entries.length; i++) {
+    if (data.entries[i].Name === event.target.textContent) {
+      console.log(data.entries[i].ID);
+      var targetUrl = encodeURIComponent('https://api-nba-v1.p.rapidapi.com/players?id=' + data.entries[i].ID);
+      xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
+      xhr.setRequestHeader('x-rapidapi-key', '1b8d6d3d44msh6a42d044a856858p1a0c39jsn1d0dcdaecf3d');
+      xhr.setRequestHeader('x-rapidapi-host', 'api-nba-v1.p.rapidapi.com');
+      xhr.responseType = 'json';
+      xhr.addEventListener('load', function () {
+        console.log((xhr.response.response[0]));
+        console.log(xhr);
+
+      });
+
+      xhr.send(data);
+    }
+  }
+}
+
+playerinfo.addEventListener('click', getInfo);
